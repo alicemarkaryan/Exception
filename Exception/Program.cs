d667feffbd43619beed51f24d4d4f6c2dad69b05
alicemarkaryan <alisa.markaryan@yahoo.com>
2022-03-29 21:04:49 +0400

    Add project files.

---
+++ Exception/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Fac(18);
+        }
+        public static int Fac(int a)
+        {
+            try
+            {
+                int b = a;
+                if (a == -1)
+
+                    return 0;
+                else
+                    return b / Fac(a - 1);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            return 1; 
+            
+            
+           
+        }
+    }
+}
